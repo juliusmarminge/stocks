993d1d435b2267c179a8c025b5126050ba4ea52c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-31 00:54:18 +0200

    initial ui

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,37 +1,33 @@
-import { type NextPage } from "next";
-import { trpc } from "../utils/trpc";
 import React from "react";
-import { UserListing } from "../components/UserListing";
+import { NextPage } from "next";
+import { CreateUser, UsersListing } from "../components/User";
 
-const Index: NextPage = () => {
-  const [inputName, setInputName] = React.useState("");
+const HomePage: NextPage = () => {
+  const [isDarkMode, setDarkMode] = React.useState(true);
+  const toggleTheme = () => {
+    setDarkMode(!isDarkMode);
+    localStorage.setItem("theme", isDarkMode ? "light" : "dark");
+    window.document.documentElement.setAttribute(
+      "data-theme",
+      isDarkMode ? "light" : "dark"
+    );
+  };
 
-  const { data: user, isLoading } = trpc.useQuery([
-    "user.getByNameWithTransactions",
-    { name: inputName },
-  ]);
-
+  const sectionStyle = "grid card bg-base-200 rounded-box place-items-center";
   return (
-    <div>
-      <p>Search user:</p>
-      <input
-        type="text"
-        className="input"
-        placeholder="Enter the user's name"
-        value={inputName}
-        onChange={(e) => setInputName(e.target.value)}
-      />
-      <h1>
-        {isLoading ? (
-          "Searching..."
-        ) : !user ? (
-          `User ${inputName} not found.`
-        ) : (
-          <UserListing user={user} />
-        )}
-      </h1>
+    <div className="flex flex-col w-4/5 mx-auto mt-10">
+      <button onClick={() => toggleTheme()}>
+        {isDarkMode ? "Set Light Theme" : "Set Dark Theme"}
+      </button>
+      <div className={sectionStyle}>
+        <UsersListing />
+      </div>
+      <div className="divider" />
+      <div className={sectionStyle}>
+        <CreateUser />
+      </div>
     </div>
   );
 };
 
-export default Index;
+export default HomePage;
