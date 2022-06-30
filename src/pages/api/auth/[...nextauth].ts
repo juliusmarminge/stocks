92dbae82b5c120b285e2d8adc995d15eb257bfb3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-30 19:46:33 +0200

    next auth trpc context

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthOptions} from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -18,4 +18,6 @@ export default NextAuth({
   pages: {
     signIn: "/auth/signin",
   },
-});
+}
+
+export default NextAuth(authOptions);
--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -2,15 +2,19 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
+import { unstable_getServerSession as getServerSession } from "next-auth";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
-export const createContext = ({
-  req,
-  res,
-}: trpcNext.CreateNextContextOptions) => {
+export const createContext = async (opts?: trpcNext.CreateNextContextOptions) => {
+  const req = opts?.req;
+  const res = opts?.res;
+
+  const session = req && res && (await getServerSession(req, res, authOptions));
   return {
     req,
     res,
     prisma,
+    session,
   };
 };
 
