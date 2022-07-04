fd3384b0e15755f9ea066b3a3ace29e52efc76c6
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-04 02:36:11 +0200

    deploy to edge

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
+    runtime: "experimental-edge",
     images: {
       allowFutureImage: true,
       remotePatterns: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,9 @@ const HomePage: NextPage = () => {
         <div className="max-w-lg">
           <h1 className="text-5xl font-bold">Welcome</h1>
           <p className="py-4">
-            <span className="italic">Stocks</span> is a simple web app to track
-            your stocks and transactions. You will be able to add transactions
-            you have made on different services and have a place to see all of
-            them together.
+            <span className="italic">Stocks</span> is a simple web app to track your
+            stocks and transactions. You will be able to add transactions you have
+            made on different services and have a place to see all of them together.
           </p>
           <p className="italic pb-4">
             App is work in progress and close-source for now.
@@ -20,6 +19,11 @@ const HomePage: NextPage = () => {
       </div>
     </div>
   );
+};
+
+export const config = {
+  unstable_runtimeJS: false,
+  runtime: "experimental-edge",
 };
 
 export default HomePage;
