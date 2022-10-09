4de4a7e7dba93146b80a4300dccb2c3b4e760001
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-09 15:25:49 +0200

    comment

--- src/server/common/gSSPPageProtection.ts
+++ src/server/common/gSSPPageProtection.ts
@@ -2,6 +2,10 @@ import { GetServerSidePropsContext } from "next";
 
 import { getServerSession } from "./getServerSession";
 
+/**
+ * Simple gSSP-Page-Protection helper that redirects
+ * the user to /auth/signin if the user is not logged in.
+ */
 export const protectPage = async (ctx: GetServerSidePropsContext) => {
   const session = await getServerSession(ctx);
   if (!session) {
